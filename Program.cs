9919365fa39b6c8d4c744e3adb8b9abb1d027d4a
Michael Bohin <mbohin@seznam.cz>
2024-05-23 16:00:53 +0200

    Logika nacitani nascrapovaneho json do hlavni metody KosikAdapteru

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿global using static System.Console;
+
+using SameProductEstimator;
+
+const string kosikProductDataRelativePath = "./../../../ProductParser/ScrapedEshopData/kosikProductData.json";
+
+string json = FileLoader.LoadFileContents(kosikProductDataRelativePath);
+
+KosikAdapter ka = new();
+
+List<NormalizedProduct> kosikProducts = ka.GetNormalizedProducts(json);	
+
+int counter = 0;
+foreach(var product in kosikProducts)
+{
+    WriteLine($"Next product {++counter}:");
+    WriteLine($"{product}\n");
+}
+
+if(kosikProducts.Count == 0)
+    WriteLine("Product list is empty.");
