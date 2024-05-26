9958b5eb18b5f65d2a8b9ffe1b3d5faff0f2f1ee
Michael Bohin <mbohin@seznam.cz>
2024-05-27 00:45:35 +0200

    Refactored FileLoader to make the main code more concise. (Renamed from FileLoader to FileHandler)

---
+++ ProductParser/Adapters/FileHandler.cs
@@ -0,0 +1,34 @@
+﻿using System.IO.Compression;
+using System.Text;
+
+namespace SameProductEstimator;
+
+internal class FileHandler
+{
+	public static string LoadJsonFromPath(string relativePath)
+	{
+		if (File.Exists(relativePath))
+		{
+			return File.ReadAllText(relativePath);
+		}
+
+		throw new FileNotFoundException(relativePath);
+	}
+
+	public static string LoadJsonFromPath(string path, string extractPath)
+	{
+		if (Directory.Exists(extractPath))
+			Directory.Delete(extractPath, true);
+
+		Directory.CreateDirectory(extractPath);
+
+		ZipFile.ExtractToDirectory(path, extractPath);
+
+		string jsonFilePath = Directory.GetFiles(extractPath, "*.json").FirstOrDefault()!;
+		string json = File.ReadAllText(jsonFilePath!, Encoding.UTF8);
+
+		Directory.Delete(extractPath, true);
+
+		return json;
+	}
+}
--- Program.cs
+++ Program.cs
@@ -3,48 +3,29 @@ using SameProductEstimator;
 using SameProductEstimator.Kosik;
 using SameProductEstimator.Rohlik;
 using SameProductEstimator.Tesco;
-using System.IO.Compression;
-using System.Text;
 
 const string kosikProductDataRelativePath = "./../../../ProductParser/ScrapedEshopData/kosikProductDataIndented.json";
 
-string json = FileLoader.LoadFileContents(kosikProductDataRelativePath);
-
-KosikAdapter ka = new();
-
-List<NormalizedProduct> kosikProducts = ka.GetNormalizedProducts(json);	
-
-ProductParserLogger.Log(kosikProducts, Eshop.Kosik);
-
-
-
 const string rohlikZipesRelativePath = "./../../../ProductParser/ScrapedEshopData/rohlikProductData.zip";
-string extractPath = @"./out/decompressedFiles/";
-if (Directory.Exists(extractPath))
-	Directory.Delete(extractPath, true);
-Directory.CreateDirectory(extractPath);
-
-ZipFile.ExtractToDirectory(rohlikZipesRelativePath, extractPath);
-
-string rohlikExtractedRelativeFilePath = $"{extractPath}rohlikProductData.json";
-
-json = File.ReadAllText(rohlikExtractedRelativeFilePath, Encoding.UTF8);
-
-Directory.Delete(extractPath, true); // clean up unziped file
-
-RohlikAdapter ra = new();
-
-List<NormalizedProduct> rohlikProducts = ra.GetNormalizedProducts(json);
-
-ProductParserLogger.Log(rohlikProducts, Eshop.Rohlik);
-
 
 const string tescoProductDataRelativePath = "./../../../ProductParser/ScrapedEshopData/tescoProductData.json";
 
-json = FileLoader.LoadFileContents(tescoProductDataRelativePath);
+const string zipExtractPath = "./out/decompressedFiles/";
 
+// Parsing Kosik products
+var json = FileHandler.LoadJsonFromPath(kosikProductDataRelativePath);
+KosikAdapter ka = new();
+List<NormalizedProduct> kosikProducts = ka.GetNormalizedProducts(json);
+ProductParserLogger.Log(kosikProducts, Eshop.Kosik);
+
+// Parsing Rohlik products
+json = FileHandler.LoadJsonFromPath(rohlikZipesRelativePath, zipExtractPath);
+RohlikAdapter ra = new();
+List<NormalizedProduct> rohlikProducts = ra.GetNormalizedProducts(json);
+ProductParserLogger.Log(rohlikProducts, Eshop.Rohlik);
+
+// Parsing Tesco products
+json = FileHandler.LoadJsonFromPath(tescoProductDataRelativePath);
 TescoAdapter ta = new();
-
 List<NormalizedProduct> tescoProducts = ta.GetNormalizedProducts(json);
-
 ProductParserLogger.Log(tescoProducts, Eshop.Tesco);
