7203695b7b8e0e2668f7ff8cb0b1e555f405444c
Michael Bohin <mbohin@seznam.cz>
2024-05-27 02:10:25 +0200

    Refactorizace metody private List<NormalizedProduct> ParseNormalizedProducts(string json) Down from 27 lines to 5

--- ProductParser/Adapters/Adapter.cs
+++ ProductParser/Adapters/Adapter.cs
@@ -1,5 +1,4 @@
-﻿using SameProductEstimator.Tesco;
-using System.Text.Json;
+﻿using System.Text.Json;
 
 namespace SameProductEstimator;
 
@@ -27,33 +26,51 @@ internal abstract class Adapter<T>
 		return ParseNormalizedProducts(json);
 	}
 
+	/// <summary>
+	/// Invalid products are not yet treated, since the product data are now 
+	/// static three files, which have been proofed to produce only valid products now
+	/// however, as we will be moving to production and run this worker automatically
+	/// we will need to implement object that will take care of invalid results 
+	/// </summary>
 	private List<NormalizedProduct> ParseNormalizedProducts(string json)
-	{ 
-		List<T> jsonProducts = JsonSerializer.Deserialize<List<T>>(json)!;
+	{
+		List<T> jsonProducts = DeserializeProducts(json);
+		var (normalizedProducts, invalidProducts) = ProcessProducts(jsonProducts);
+		
+		LogProductCounts(normalizedProducts, jsonProducts.Count, invalidProducts.Count);
+		return normalizedProducts;
+	}
 
+	private static List<T> DeserializeProducts(string json) => JsonSerializer.Deserialize<List<T>>(json)!;
+
+	private (List<NormalizedProduct>, List<T>) ProcessProducts(List<T> products)
+	{
 		List<NormalizedProduct> normalizedProducts = [];
-		List<T> invalidJsonProducts = [];
+		List<T> invalidProducts = [];
 
-		foreach (T jsonProduct in jsonProducts)
+		foreach (T product in products)
 		{
-			if (TryGetNormalized(jsonProduct, out NormalizedProduct normalizedProduct))
+			if (TryGetNormalized(product, out NormalizedProduct? normalizedProduct))
 			{
-				normalizedProducts.Add(normalizedProduct);
+				normalizedProducts.Add(normalizedProduct!);
 			} else
 			{
-				invalidJsonProducts.Add(jsonProduct);
+				invalidProducts.Add(product);
 			}
 		}
 
-		WriteLine(GetNameOf());
-		WriteLine(jsonProducts.Count);
-		WriteLine($"Normalized products: {normalizedProducts.Count}");
-		WriteLine($"Invalid: {invalidJsonProducts.Count}");
-		WriteLine($"{normalizedProducts.Count} + {invalidJsonProducts.Count} = {normalizedProducts.Count + invalidJsonProducts.Count}\n");
+		return (normalizedProducts, invalidProducts);
+	}
 
+	private void LogProductCounts(List<NormalizedProduct> normalizedProducts, int total, int invalidCount)
+	{
 		ProductParserLogger.Log(normalizedProducts, GetEshopType());
 
-		return normalizedProducts;
+		WriteLine(GetNameOf());
+		WriteLine(total);
+		WriteLine($"Normalized products: {normalizedProducts.Count}");
+		WriteLine($"Invalid products: {invalidCount}");
+		WriteLine($"{normalizedProducts.Count} + {invalidCount} = {total}\n");
 	}
 
 	protected bool TryGetNormalized(T product, out NormalizedProduct normalizedProduct)
