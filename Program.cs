9ebf77d7d7dad44aceb7fe0013d1df4fd4337028
Michael Bohin <mbohin@seznam.cz>
2024-06-03 03:12:12 +0200

    Serilog usage intro sample

--- Program.cs
+++ Program.cs
@@ -4,6 +4,68 @@ using SameProductEstimator.Kosik;
 using SameProductEstimator.Rohlik;
 using SameProductEstimator.Tesco;
 using System.Diagnostics;
+using Serilog;
+using Serilog.Settings.Configuration;
+using Serilog.Formatting.Json;
+using Serilog.Core;
+using Serilog.Sinks.SystemConsole.Themes;
+using Serilog.Events;
+using Microsoft.Extensions.Configuration;
+using System.Text.Json;
+using Microsoft.Extensions.Logging;
+using SerilogTimings;
+using SerilogTimings.Extensions;
+
+/*/var configuration = new ConfigurationBuilder()
+	.SetBasePath(AppDomain.CurrentDomain.BaseDirectory) // Optional based on setup
+	.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+	.Build();/**/
+
+Serilog.ILogger logger = new LoggerConfiguration()
+	// .ReadFrom.Configuration(configuration)
+	.MinimumLevel.Debug()
+	.Destructure.ByTransforming<NormalizedProduct>(x => new {
+		x.Name,	x.Price, x.Eshop
+	})
+	.WriteTo.Console(theme: AnsiConsoleTheme.Code)
+	.WriteTo.File("log.txt", rollingInterval: RollingInterval.Day, rollOnFileSizeLimit: true)
+	
+	.CreateLogger();
+
+Log.Logger = logger;
+
+
+var timedOp = logger.BeginOperation("Initiating intro to serilog section");
+Log.Error("Hello, Serilog!");
+Log.Debug("Debug log reporting");
+
+int i = 5;
+string hello = "hi";
+Log.Information("Hello world! {i} {hello}", i, hello);
+
+var name = "Michael";
+var age = 33;
+
+Log.Information("{Name} just turned: {Age}", name, age);
+
+NormalizedProduct sampleProduct = new("Jogobela jahoda 150g", "/hezkeUrlJahody", 25.9m, Eshop.Kosik);
+Log.Information("Uzivatel si vyzadal produkt:\n{@Product}", sampleProduct );
+
+await Task.Delay(138);
+
+Random rand = new();
+if(rand.Next(2) == 0)
+{
+	timedOp.Complete();
+} else
+{
+	timedOp.Abandon();
+}
+
+
+Log.CloseAndFlush();
+
+return;
 
 Stopwatch sw = Stopwatch.StartNew();
 
